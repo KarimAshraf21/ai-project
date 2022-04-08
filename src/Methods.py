d38a42b580ddcafbf19428fd2a47e484161e26e3
karim <karimashraf0055@gmail.com>
2022-04-08 12:53:38 +0200

    bfs version 1 (no path)

---
+++ src/Methods.py
@@ -0,0 +1,38 @@
+import networkx as nx
+from queue import Queue
+
+
+def BFS(graph, start_node, goal_node):
+    visited = []
+    fringe = Queue()
+    path = []
+    current_node = start_node
+    found=False
+
+    fringe.put(current_node)
+    while not fringe.empty():
+        print(f"fringe: {fringe.queue}")
+        current_node=fringe.get()
+        print(f"current node {current_node}")
+        if (current_node==goal_node):
+            print("goal is ",current_node)
+            visited.append(current_node)
+            found=True
+            break
+        else:
+            visited.append(current_node)
+            neighbors_iter=graph.neighbors(current_node)
+            neighbors=list(neighbors_iter)
+            for neighbor in neighbors:
+                print(f"neighbor {neighbor}")
+                if neighbor in visited:
+                    continue
+                else:
+                    fringe.put(neighbor)
+            neighbors.clear()
+
+    if found:
+        print("goal found")
+    else:
+        print('not found')
+    print(visited)
