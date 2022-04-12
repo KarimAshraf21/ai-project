7adb49208871b1ab82aa449d5d87d313f5fe0260
Ziad Assem <ziad.assem2001@gmail.com>
2022-04-12 21:03:44 +0200

    test

--- src/Methods.py
+++ src/Methods.py
@@ -7,22 +7,22 @@ def BFS(graph, start_node, goal_node):
     fringe = Queue()
     path = []
     current_node = start_node
-    found=False
+    found = False
 
     fringe.put(current_node)
     while not fringe.empty():
         print(f"fringe: {fringe.queue}")
-        current_node=fringe.get()
+        current_node = fringe.get()
         print(f"current node {current_node}")
-        if (current_node==goal_node):
-            print("goal is ",current_node)
+        if (current_node == goal_node):
+            print("goal is ", current_node)
             visited.append(current_node)
-            found=True
+            found = True
             break
         else:
             visited.append(current_node)
-            neighbors_iter=graph.neighbors(current_node)
-            neighbors=list(neighbors_iter)
+            neighbors_iter = graph.neighbors(current_node)
+            neighbors = list(neighbors_iter)
             for neighbor in neighbors:
                 print(f"neighbor {neighbor}")
                 if neighbor in visited:
