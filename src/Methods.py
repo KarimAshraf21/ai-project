857934f0ce51af1886739f90e1043f244fe1d790
karim <karimashraf0055@gmail.com>
2022-04-21 16:09:49 +0200

    added comments for readability

--- src/Methods.py
+++ src/Methods.py
@@ -3,18 +3,20 @@ from queue import Queue
 
 
 def BFS(graph, start_node, goal_node):
+    # some variables
     visited = []
     fringe = Queue()
     path = []
     current_node = start_node
+    # dictionary that keeps track of parents to find path
     parent = dict()
     parent[start_node] = None
-
     found = False
 
     fringe.put(current_node)
     while not fringe.empty():  # iterates until no nodes left to visit
         print(f"fringe: {fringe.queue}")
+        # the node that should be expanded is the node first node in the queue fringe
         current_node = fringe.get()
         print(f"current node {current_node}")
         if current_node == goal_node:
@@ -23,16 +25,21 @@ def BFS(graph, start_node, goal_node):
             found = True
             break
         else:
+            # getting neighbors and adding them to the visited list
             visited.append(current_node)
             neighbors_iter = graph.neighbors(current_node)
             neighbors = list(neighbors_iter)
             print(neighbors)
             print(parent)
-            for neighbor in neighbors:  # problem with appending value
+
+            # assigning parents to nodes
+            for neighbor in neighbors:
                 if neighbor in parent.keys():
                     list(parent[neighbor]).extend(current_node)
                 else:
-                    parent[neighbor]=(current_node)
+                    parent[neighbor] = current_node
+
+            # putting unvisited nodes in the fringe
             for neighbor in neighbors:
                 print(f"neighbor {neighbor}")
                 if neighbor in visited:
@@ -44,6 +51,7 @@ def BFS(graph, start_node, goal_node):
     if found:
         print("goal found")
         '''path(goal_node)'''
+        # backtracking for getting the parents which are the path
         path.append(goal_node)
         while parent[current_node] is not None:
             path.append(parent[current_node])
@@ -51,6 +59,8 @@ def BFS(graph, start_node, goal_node):
         path.reverse()
     else:
         print('not found')
+
+
     print(f"visited is {visited}")
     print(parent)
     print(f"path is {path}")
--- src/main_test.py
+++ src/main_test.py
@@ -7,13 +7,15 @@ e = [('a', 'c'), ('a', 'e'), ('c', 'b'), ('b', 'e'),('e', 'f'),('e', 'd')]
 g=nx.MultiDiGraph()
 g.add_edges_from(e)
 
-nx.draw(g, pos=nx.spring_layout(g),with_labels=True)
+
 
 print(g.adj)
 
 path=m.BFS(g,'a','b')
-
+subax1 = plt.subplot(121)
+nx.draw(g, pos=nx.spring_layout(g),with_labels=True)
 path_graph=nx.DiGraph()
 path_graph.add_nodes_from(path)
+subax2 = plt.subplot(122)
 nx.draw(path_graph,with_labels=True)
 plt.show()
